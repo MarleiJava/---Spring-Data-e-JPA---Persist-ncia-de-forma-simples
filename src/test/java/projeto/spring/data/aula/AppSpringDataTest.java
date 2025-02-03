44c11037f2b91c4bd66140fa0bf724c363b2dbff
marlei <marleirds@outlook.com>
2025-02-02 23:06:47 -0300

    Inserção e consulta tudo OK:
    https://projetojavaweb.com/certificado-aluno/plataforma-curso/aulaatual/51d25b2f-34f5-4752-a0d2-76dd1d05bd8e/idcurso/1/idvideoaula/668

--- src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -1,17 +1,56 @@
 package projeto.spring.data.aula;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
+import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 public class AppSpringDataTest {
 	
+	@Autowired
+	private InterfaceSpringDataUser interfaceSpringDataUser;
+	
 	@Test
 	public void tetInsert() {
 		System.out.println("Iniciou spring com sucesso");
 	}
+	
+	
+	public void testInsert() {
+		
+
+		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
+		usuarioSpringData.setEmail("doguinho@outlook.com");
+		usuarioSpringData.setIdade(10);
+		usuarioSpringData.setLogin("Dog");
+		usuarioSpringData.setNome("Hot dog ");
+		usuarioSpringData.setSenha("123");
+		interfaceSpringDataUser.save(usuarioSpringData);
+	
+	}
+	
+	@Test
+	public void testConsulta() {
+		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(1L);
+		
+		if(usuarioSpringData == null) {
+			System.out.println("Não tem nada cadastrado");
+		}else {
+		
+		System.out.println(usuarioSpringData.get().getNome());
+		System.out.println(usuarioSpringData.get().getIdade());
+		System.out.println(usuarioSpringData.get().getEmail());
+		System.out.println(usuarioSpringData.get().getLogin());
+		}
+	}
+	
 
 }
